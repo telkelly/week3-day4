a24b0ae73e85468dd52583afaed613f38284c065
Kelly <84016657+telkelly@users.noreply.github.com>
2023-03-29 12:14:05 +0300

    added index.html and script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// exercise-1
+
+let a = 3;
+let b = 38;
+
+if (a > b) {
+    console.log(`The biggest number is ${a}`)
+} else {
+    console.log(`The biggest number is ${b}`);
+}
+
+// exercise-2
+
