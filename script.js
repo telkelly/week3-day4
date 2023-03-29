24529915e862f346119f8d9c898b7da8643a0588
Kelly <84016657+telkelly@users.noreply.github.com>
2023-03-29 12:27:15 +0300

    did 3 exercises

--- script.js
+++ script.js
@@ -11,3 +11,27 @@ if (a > b) {
 
 // exercise-2
 
+let newDog = 'Chihuahua';
+
+console.log(newDog.length)
+
+console.log(newDog.toUpperCase())
+console.log(newDog.toLowerCase())
+
+if (newDog == 'Chihuahua') {
+    console.log("I love Chihuahuas, it's my favorite dog breed(it's lie)");
+} else {
+    console.log("I don't care, i prefer cats")
+}
+
+// exercise-3
+
+let getNum = prompt('Give me a number');
+
+let num = parseFloat(getNum);
+
+if (num % 2 == 0) {
+    console.log(`${num} is an even number`)   
+} else {
+     console.log(`${num} is an odd number`);
+}
