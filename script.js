00b1a81f2328613c66ebb7e690e95531990d8fb5
Kelly <84016657+telkelly@users.noreply.github.com>
2023-03-29 23:25:44 +0300

    added 4th exercise

--- script.js
+++ script.js
@@ -26,12 +26,26 @@ if (newDog == 'Chihuahua') {
 
 // exercise-3
 
-let getNum = prompt('Give me a number');
+// let getNum = prompt('Give me a number');
 
-let num = parseFloat(getNum);
+// let num = parseFloat(getNum);
 
-if (num % 2 == 0) {
-    console.log(`${num} is an even number`)   
-} else {
-     console.log(`${num} is an odd number`);
+// if (num % 2 == 0) {
+//     console.log(`${num} is an even number`)   
+// } else {
+//      console.log(`${num} is an odd number`);
+// }
+
+// exercise-4
+
+const users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
+
+if (users.length == 0) {
+    console.log('No one is online')
+} else if (users.length == 1) {
+    console.log(`${users[0]} is online`)
+} else if (users.length == 2) {
+    console.log(`${users[0]} and ${users[1]} are online`);
+} else if (users.length > 2) {
+    console.log(`${users[0]}, ${users[1]} and 3 more are online`);
 }
