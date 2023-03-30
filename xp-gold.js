39954da19e51cf2079a1d71dc0502193f88244d8
Kelly <84016657+telkelly@users.noreply.github.com>
2023-03-30 20:03:39 +0300

    dobe xp-gold exercises

--- xp-gold.js
+++ xp-gold.js
@@ -21,3 +21,28 @@
 
 // exercise-2
 
+// let grade = prompt("What is your grade?");
+
+// let gradeNum = Number(grade)
+
+// if (grade > 90) {
+//   console.log("A");
+// } else if (grade > 80 && grade < 90) {
+//   console.log("B");
+// } else if (grade > 70 && grade < 80) {
+//   console.log("C");
+// } else {
+//     console.log('D')
+// }
+
+// exercise-3
+
+let verb = prompt("Give me any verb");
+
+if (verb.length >=3 && verb.includes('ing')) {
+    alert(verb.concat('ly'))
+}else if(verb.length >=3 ){ 
+    alert(verb.concat('ing'))
+}else if (verb.length < 3) {
+   alert(verb)
+}
