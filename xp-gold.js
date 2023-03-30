26b13157eebce7ee95c734257901aa3621403f07
Kelly <84016657+telkelly@users.noreply.github.com>
2023-03-30 19:17:17 +0300

    making xp-gold

---
+++ xp-gold.js
@@ -0,0 +1,23 @@
+// exercise-1
+
+// let question = prompt('Which language do you speak?');
+
+// let answer = question.toLowerCase();
+
+// switch (answer) {
+//   case "english":
+//     alert("Hello");
+//     break;
+//   case "french":
+//     alert("Bonjour");
+//     break;
+//   case "hebrew":
+//     alert("Shalom");
+//     break;
+//     default:
+//      alert("01110011 01101111 01110010 01110010 01111001");
+//     break;
+// }
+
+// exercise-2
+
